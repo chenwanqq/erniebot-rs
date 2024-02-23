e650aaa2f08454825af28987e0de41328940a0c0
chenwanqq <wanqi0605@qq.com>
2024-02-23 17:52:54 +0800

    WIP

--- src/option.rs
+++ src/option.rs
@@ -1,18 +1,11 @@
 use serde::{Deserialize, Serialize};
+use strum_macros::EnumString;
 
 #[derive(Debug, Clone, Serialize, Deserialize, PartialEq)]
 #[serde(rename_all(serialize = "snake_case", deserialize = "snake_case"))]
 pub enum ResponseFormat {
     JsonObject,
     Text,
-}
-impl ToString for ResponseFormat {
-    fn to_string(&self) -> String {
-        match self {
-            ResponseFormat::JsonObject => "json_object".to_string(),
-            ResponseFormat::Text => "text".to_string(),
-        }
-    }
 }
 
 #[derive(Debug, Clone, Serialize, Deserialize, PartialEq)]
@@ -29,4 +22,68 @@ pub enum ChatOpt {
     MaxOutputTokens(u32),
     ResponseFormat(ResponseFormat),
     UserId(String),
+}
+
+/* below are options for text2image */
+
+#[derive(Debug, Clone, Serialize, Deserialize, PartialEq, Default, EnumString)]
+pub enum Size {
+    #[default]
+    #[strum(serialize = "1024x1024")]
+    S1024x1024,
+    #[strum(serialize = "768x768")]
+    S768x768,
+    #[strum(serialize = "768x1024")]
+    S768x1024,
+    #[strum(serialize = "1024x768")]
+    S1024x768,
+    #[strum(serialize = "576x1024")]
+    S576x1024,
+    #[strum(serialize = "1024x576")]
+    S1024x576,
+}
+
+#[derive(Debug, Clone, Serialize, Deserialize, PartialEq, Default, EnumString)]
+pub enum SamplerIndex {
+    #[default]
+    #[strum(serialize = "Euler a")]
+    EulerA,
+    #[strum(serialize = "Euler")]
+    Euler,
+    #[strum(serialize = "DPM++ 2M")]
+    DPMpp2M,
+    #[strum(serialize = "DPM++ 2M Karras")]
+    DPMpp2MKarras,
+    #[strum(serialize = "LMS Karras")]
+    LMSKarras,
+    #[strum(serialize = "DPM++ SDE")]
+    DPMppSDE,
+    #[strum(serialize = "DPM++ SDE Karras")]
+    DPMppSDEKarras,
+    #[strum(serialize = "DPM2 a Karras")]
+    DPM2AKarras,
+    #[strum(serialize = "Heun")]
+    Heun,
+    #[strum(serialize = "DPM++ 2M SDE")]
+    DPMpp2MSDE,
+    #[strum(serialize = "DPM++ 2M SDE Karras")]
+    DPMpp2MSDEKarras,
+    #[strum(serialize = "DPM2")]
+    DPM2,
+    #[strum(serialize = "DPM2 Karras")]
+    DPM2Karras,
+    #[strum(serialize = "DPM2 a")]
+    DPM2A,
+    #[strum(serialize = "LMS")]
+    LMS,
+}
+
+#[derive(Debug, Clone, Serialize, Deserialize, PartialEq)]
+#[serde(rename_all(serialize = "snake_case", deserialize = "snake_case"))]
+pub enum Text2ImageOpt {
+    Prompt(String),
+    NegativePrompt(String),
+    Size(Size),
+    N(u32),     //1-4
+    Steps(u32), //10-50
 }
