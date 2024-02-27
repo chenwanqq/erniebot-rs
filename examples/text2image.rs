84767f7305d99f00342b0a485bd3b03169ff113e
chenwanqq <wanqi0605@qq.com>
2024-02-27 16:53:10 +0800

    fix minor problem in example

--- examples/text2image.rs
+++ examples/text2image.rs
@@ -3,16 +3,15 @@ use erniebot_rs::utils::base64_to_image;
 fn main() {
     let text2image = Text2ImageEndpoint::new(Text2ImageModel::StableDiffusionXL).unwrap();
     let prompt = "A beautiful sunset over the ocean".to_string();
-    let mut options = Vec::new();
-    options.push(Text2ImageOpt::Style(Style::DigitalArt));
-    options.push(Text2ImageOpt::Size(Size::S1024x768));
+    let options = vec![
+        Text2ImageOpt::Style(Style::DigitalArt),
+        Text2ImageOpt::Size(Size::S1024x768),
+    ];
     let text2image_response = text2image.invoke(prompt, options).unwrap();
     let image_results = text2image_response.get_image_results().unwrap();
-    let mut index = 0;
-    for image_string in image_results {
+    for (index, image_string) in image_results.into_iter().enumerate() {
         let image = base64_to_image(image_string).unwrap();
         let filepath = format!("./tmp/image_{}.png", index);
         image.save(filepath).unwrap();
-        index += 1;
     }
 }
